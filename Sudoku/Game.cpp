c032ef8434f83db92f805ed5658aec4cb1f26580
Jongmin Han <33759102+jongminHan@users.noreply.github.com>
2019-08-09 21:30:02 +0900

    Implement GUI(still in progress)

--- Sudoku/Game.cpp
+++ Sudoku/Game.cpp
@@ -11,9 +11,50 @@ bool Game::Init()
 
 	// Initialising seed for random number generation
 	srand(static_cast<unsigned int>(time(nullptr)));
+
+	tgui::EditBox::Ptr sudokuArray[81];
+	tgui::EditBox::Ptr editBox;
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			editBox = tgui::EditBox::create();
+			editBox->setPosition(150 + 60 * i, 60 + 60 * j);
+			editBox->setSize(50, 50);
+			sudokuArray[i * 9 + j] = editBox;
+		}
+	}
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			mGui->add(sudokuArray[i * 9 + j]);
+		}
+	}
+
 	return true;
 }
 
 void Game::Run()
 {
+
+	while (mWindow.isOpen())
+	{
+		sf::Event event;
+		while (mWindow.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				mWindow.close();
+			}
+
+			mGui->handleEvent(event); // Pass the event to the widgets
+		}
+
+		mWindow.clear();
+		mGui->draw(); // Draw all widgets
+		mWindow.display();
+	}
 }
